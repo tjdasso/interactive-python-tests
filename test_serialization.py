61700bd0634049e89b8bcd1ae790d27da2ca066c
TJ Dasso <tjdasso@gmail.com>
2019-12-10 21:14:54 -0500

    Iteratively Perform Serializations

--- test_serialization.py
+++ test_serialization.py
@@ -7,70 +7,61 @@ from test_functions import *
 # FUNCTION 1
 print("Function 1")
 begin = time.time()
-pack_apply_message(func1, args1, kwargs1, buffer_threshold=1024*1024, item_threshold=1024)
+for i in range(100):
+    pack_apply_message(func1, args1, kwargs1, buffer_threshold=1024*1024, item_threshold=1024)
 end = time.time()
-print("Ipyparallel.Serialize: {:2f}".format(end - begin))
+print("Ipyparallel.Serialize: {:2f}".format((end - begin) / 100))
 
 begin = time.time()
-dill.dumps(func1)
+for i in range(100):
+    dill.dumps(func1)
 end = time.time()
-print("Dill: {:2f}".format(end - begin))
+print("Dill: {:2f}".format((end - begin) / 100))
 
 begin = time.time()
-inspect.getsource(func1)
+for i in range(100):
+    inspect.getsource(func1)
 end = time.time()
-print("Inspect: {:2f}".format(end - begin))
+print("Inspect: {:2f}".format((end - begin) / 100))
 print()
 
 # FUNCTION 2
 print("Function 2")
 begin = time.time()
-pack_apply_message(func2, args2, kwargs2, buffer_threshold=1024*1024, item_threshold=1024)
+for i in range(100):
+    pack_apply_message(func2, args2, kwargs2, buffer_threshold=1024*1024, item_threshold=1024)
 end = time.time()
-print("Ipyparallel.Serialize: {:2f}".format(end - begin))
+print("Ipyparallel.Serialize: {:2f}".format((end - begin) / 100))
 
 begin = time.time()
-dill.dumps(func2)
+for i in range(100):
+    dill.dumps(func2)
 end = time.time()
-print("Dill: {:2f}".format(end - begin))
+print("Dill: {:2f}".format((end - begin) / 100))
 
 begin = time.time()
-inspect.getsource(func2)
+for i in range(100):
+    inspect.getsource(func2)
 end = time.time()
-print("Inspect: {:2f}".format(end - begin))
+print("Inspect: {:2f}".format((end - begin) / 100))
 print()
 
 # FUNCTION 3
 print("Function 3")
 begin = time.time()
-pack_apply_message(func3, args3, kwargs3, buffer_threshold=1024*1024, item_threshold=1024)
+for i in range(100):
+    pack_apply_message(func3, args3, kwargs3, buffer_threshold=1024*1024, item_threshold=1024)
 end = time.time()
-print("Ipyparallel.Serialize: {:2f}".format(end - begin))
+print("Ipyparallel.Serialize: {:2f}".format((end - begin) / 100))
 
 begin = time.time()
-dill.dumps(func3)
+for i in range(100):
+    dill.dumps(func3)
 end = time.time()
-print("Dill: {:2f}".format(end - begin))
+print("Dill: {:2f}".format((end - begin) / 100))
 
 begin = time.time()
-inspect.getsource(func3)
+for i in range(100):
+    inspect.getsource(func3)
 end = time.time()
-print("Inspect: {:2f}".format(end - begin))
-print()
-
-# FUNCTION 4
-print("Function 4")
-begin = time.time()
-pack_apply_message(func4, args4, kwargs4, buffer_threshold=1024*1024, item_threshold=1024)
-end = time.time()
-print("Ipyparallel.Serialize: {:2f}".format(end - begin))
-
-begin = time.time()
-dill.dumps(func4)
-end = time.time()
-print("Dill: {:2f}".format(end - begin))
-
-begin = time.time()
-inspect.getsource(func4)
-end = time.time()
-print("Inspect: {:2f}".format(end - begin))
+print("Inspect: {:2f}".format((end - begin) / 100))
